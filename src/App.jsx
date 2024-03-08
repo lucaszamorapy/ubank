38260f5f44d31a4da0a9c9edab33baa8cc05a3a5
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-03-08 12:09:20 -0300

    feat: route home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Home from "./pages/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
